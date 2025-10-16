653b80cef04d37e51f5de81ab29c9de79f6e4938
r.cosiocastro <r.cosiocastro@outlook.com>
2025-10-15 17:08:20 -0700

    Cambio de versión para actualización en dispositivos.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.rafaelcosio.gpslivestock"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val mapsApiKey = getLocalProperty("MAPS_API_KEY")
